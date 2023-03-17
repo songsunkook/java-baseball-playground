300508b286a9c9001451e9696824e893742736c8
songsunkook <songsunkook@gmail.com>
2023-03-17 15:28:55 +0900

    1-0. 학습 테스트 실습

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,29 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("요구사항 1")
+    void test1(){
+        assertThat("1,2".split(",")).contains("1","2");
+        assertThat("1".split(",")).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("요구사항 2")
+    void test2(){
+        assertThat("(1,2)".substring(1,"(1,2)".length()-1)).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("요구사항 3")
+    void test3(){
+        int idx = 3;
+        try {
+            System.out.println("abc".charAt(idx));
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("ERROR : " + e.getMessage());
+        }
     }
 }
